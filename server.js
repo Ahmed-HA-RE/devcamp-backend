c0cdee0bd73623f02019b183f187afa087ccdfaf
Ahmed-HA-RE <ah607k@gmail.com>
2025-09-22 21:28:49 +0400

    feat:add logger middleware

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bootcampsRoute from './routes/bootcamps.js';
+import { logger } from './middleware/logger.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(logger);
 app.use('/api/v1/bootcamps', bootcampsRoute);
 
 app.listen(PORT, () =>
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port:${PORT} `
+    `Server is running in ${process.env.NODE_ENV} mode on port:${PORT}`
   )
 );
